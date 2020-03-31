556efe7cb2edb39ae55a3087cda8bda3484013eb
RT-Colby <cfrey@protonmail.com>
2020-03-30 17:41:27 -0700

    presaeson deleteion

--- LOG4/contains.py
+++
@@ -1,7 +0,0 @@
-dayBadge = open('/home/colby/Downloads/Badge/Badge 4th.txt','r')
-badgeNames = open('/home/colby/Downloads/Badge/loginNames/SwipeIns','w+')
-
-
-for line in dayBadge:
-	if 'Swipe In' in line:
-		badgeNames.write(line)
